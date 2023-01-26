dbebede6e2c8784a5e071430f7d3207be035e361
codbain <kikerboxing@gmail.com>
2023-01-26 11:10:16 +0200

    add files

--- WP/childhood/functions.php
+++ WP/childhood/functions.php
@@ -6,7 +6,14 @@ function childhood_scripts(){
 
     wp_enqueue_script('childhood-scrip', get_template_directory_uri() . '/assets/js/main.min.js', array(), null, true);
 }
-
+;
+add_theme_support( 'custom-logo', [
+    'height'      => 190,
+    'width'       => 190,
+    'flex-width'  => false,
+    'flex-height' => false,
+    'header-text' => '',
+] );
 
 
 
